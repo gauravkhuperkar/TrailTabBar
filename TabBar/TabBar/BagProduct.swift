55280932ae2a0d5d0b9f348282657c834063e05b
Gaurav k <gauravk@in-gaurav.local>
2016-05-12 16:18:46 +0530

    made lot of changes

---
+++ TabBar/TabBar/BagProduct.swift
@@ -0,0 +1,13 @@
+//
+//  BagProduct.swift
+//  TabBar
+//
+//  Created by Gaurav k on 5/11/16.
+//  Copyright © 2016 Gaurav k. All rights reserved.
+//
+
+import UIKit
+
+class BagProduct: TableVi {
+
+}
